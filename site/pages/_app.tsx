ff86c2bd041493a266083708fa9242844d8b43de
Юрий <y.headman@ya.ru>
2023-02-27 00:20:49 +0300

    Test cms

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -6,14 +6,26 @@ import { FC, ReactNode, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import swell from 'swell-node'
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = (Component as any).Layout || Noop
+  const Layout = (Component as any).Layout || Noop 
 
   useEffect(() => {
     document.body.classList?.remove('loading')
+    swell.init('test-cms-store', 'WvjpHjyP22ieScAfCqIfIQE5sZhRIf0K', {cache: false})
+    const getProducts = async () => {
+      try {
+        const products = await swell.get('/products');
+        console.log(products);
+        return products;
+      } catch (e) {
+        console.error(e.message)
+      }
+    };
+    getProducts()
   }, [])
 
   return (
